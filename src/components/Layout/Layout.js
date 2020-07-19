a85b5f08ad0713462d0135c6a4beb722fce8759d
omar montoya <montoyaomar01@hotmail.com>
2020-07-19 18:39:04 -0500

    add component of burger ingredients with propTypes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Aux   from '../../hoc/Aux';
+import React   from 'react';
+import Aux     from '../../hoc/Aux';
+import './Layout.css';
 
 const layout = ( props ) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className="content">
       { props.children }
     </main>
   </Aux>
